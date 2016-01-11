082c272fed4228f6ea8082d842ce55801e4368d1
Omerhanay <omerhanay@hotmail.com>
2016-01-11 12:36:37 +0200

    Code and connect

---
+++ derece/derece.ino
@@ -0,0 +1,18 @@
+#include <LiquidCrystal.h> 
+LiquidCrystal lcd(12,11,5,4,3,2);
+int sicaklik =0; 
+const int sicaklik_pin= A0; 
+
+void setup() {
+lcd.begin(16,2);
+}
+
+void loop() {
+lcd.clear();
+sicaklik = analogRead(sicaklik_pin); 
+sicaklik = sicaklik * 0.48828125; //Celcius cinsinden sıcaklık birimine dönüştürdük.
+lcd.print("Sicaklik: "); 
+lcd.print(sicaklik); 
+lcd.print("C"); 
+delay(1000);
+}
